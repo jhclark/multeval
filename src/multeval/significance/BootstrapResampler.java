2c48a0185f0e3ed0270af053f6cf5a877a402a2f
Jonathan Clark <jon.h.clark@gmail.com>
2011-04-26 13:59:49 -0400

    Add significance testing code (bootstrap resampling/AR)

---
+++ src/multeval/significance/BootstrapResampler.java
@@ -0,0 +1,158 @@
+package multeval.significance;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.base.Preconditions;
+
+import metrics.Metric;
+import multeval.util.ArrayUtils;
+import multeval.util.MathUtils;
+import multeval.util.SuffStatUtils;
+
+public class BootstrapResampler {
+
+	private final Random random = new Random();
+	private final List<Metric> metrics;
+	private final List<List<double[]>> suffStats;
+	private int totalDataPoints;
+
+	/**
+	 * 
+	 * @param suffStats
+	 *            First list corresponds to the metrics, the
+	 *            second dimension is number of data points (i.e. sentences)
+	 *            and the inner array is the sufficient statistics for each metric.
+	 */
+	public BootstrapResampler(List<Metric> metrics,
+			List<List<double[]>> suffStats) {
+		
+		Preconditions.checkArgument(metrics.size() > 0, "Must have at least one metric.");
+		Preconditions.checkArgument(suffStats.size() > 0, "Must have at least one data point.");
+		// TODO: Check for sufficient stats and metric count under each data point being parallel
+		
+		this.metrics = metrics;
+		this.suffStats = suffStats;
+		this.totalDataPoints = suffStats.get(0).size();
+		
+		Preconditions.checkArgument(totalDataPoints > 0, "Need more than zero data points.");
+	}
+
+	/**
+	 * Returns a list whose size corresponds to the number of metrics being used
+	 * for this resampler. Each inner double array contains a n metric values,
+	 * calculated by applying the metric to the sufficient statistics aggregated
+	 * from each resampling.
+	 * 
+	 * @param sampleSize
+	 *            Size of each of the n re-sampled groups taken from the set of
+	 *            points passed to the contructor.
+	 * @param numSamples
+	 *            Number of resampled groups to be drawn.
+	 * @return
+	 */
+	public List<double[]> resample(int numSamples) {
+		
+		List<double[]> metricValues = new ArrayList<double[]>(metrics.size());
+		for(int i=0; i<metrics.size(); i++) {
+			metricValues.add(new double[numSamples]);
+		}
+		
+		int sampleSize = totalDataPoints;
+		int[] sampleMembers = new int[sampleSize];
+		for (int iSample = 0; iSample < numSamples; iSample++) {
+			chooseSampleMembers(totalDataPoints, sampleMembers);
+			// NOTE: We could dump the sample members for analysis here if we wanted
+			for(int iMetric = 0; iMetric < metrics.size(); iMetric++) {
+				double[] summedStats = sumStats(sampleMembers, iMetric, suffStats);
+				Metric metric = metrics.get(iMetric);
+				double score = metric.score(summedStats);
+				metricValues.get(iMetric)[iSample] = score;
+			}
+		}
+		return metricValues;
+	}
+
+	private static double[] sumStats(int[] sampleMembers,
+			int iMetric, List<List<double[]>> suffStats) {
+		
+		int numMetricStats = suffStats.get(iMetric).get(0).length;
+		double[] summedStats = new double[numMetricStats];
+
+		for(int dataIdx : sampleMembers) {
+			ArrayUtils.plusEquals(summedStats, suffStats.get(iMetric).get(dataIdx));
+		}
+		return summedStats;
+	}
+
+	private void chooseSampleMembers(int totalPointsAvailable, int[] sampleMembersOut) {
+		for(int i = 0; i<sampleMembersOut.length; i++) {
+			sampleMembersOut[i] = random.nextInt(totalPointsAvailable);
+		}
+	}
+	
+	private static void saveSamples(File scoresOut, double[] scores) throws FileNotFoundException {
+		PrintWriter out = new PrintWriter(scoresOut);
+		for(double score : scores) {
+			out.println(score);
+		}
+		out.close();
+		System.err.println("Saved " + scores.length + " resampled metric scores to " + scoresOut.getAbsolutePath());
+	}
+
+	private static void printOverallInfo(String metricName, Metric metric, List<double[]> stats) {
+		double[] summedStats = SuffStatUtils.sumStats(stats);
+		double score = metric.score(summedStats);
+		System.err.println("Overall " + metricName + " stats: score = " + score*100);
+	}
+	
+	private static void printResampleStats(String metricName, double[] samples) {
+		double mean = MathUtils.average(samples)*100;
+		double stddev = Math.sqrt(MathUtils.variance(samples))*100;
+		double min = MathUtils.min(samples)*100;
+		double max = MathUtils.max(samples)*100;
+		System.err.printf("Resampled %s stats: mean = %.2f stddev = %.2f min = %.2f max = %.2f\n", metricName, mean, stddev, min, max);
+	}
+	
+//	public static void main(String[] args) throws IOException {
+//		if(args.length != 7) {
+//			System.err.println("Usage: program bleuSuffstatsIn meteorSuffstatsIn terSuffstatsIn bleuScoresOut meteorScoresOut terScoresOut numSamples");
+//			System.exit(1);
+//		}
+//		File bleuStatsIn = new File(args[0]);
+//		File meteorStatsIn = new File(args[1]);
+//		File terStatsIn = new File(args[2]);
+//		File bleuScoresOut = new File(args[3]);
+//		File meteorScoresOut = new File(args[4]);
+//		File terScoresOut = new File(args[5]);
+//		int numSamples = Integer.parseInt(args[6]);
+//		
+//		List<Metric> metrics = Arrays.asList(new BLEU(), new METEOR(METEOR.RANKING_EN_WEIGHTS), new TER());
+//		List<List<double[]>> suffStats = new ArrayList<List<double[]>>(metrics.size());
+//		suffStats.add(SuffStatUtils.loadSuffStats(bleuStatsIn));
+//		suffStats.add(SuffStatUtils.loadSuffStats(meteorStatsIn));
+//		suffStats.add(SuffStatUtils.loadSuffStats(terStatsIn));
+//		
+//		printOverallInfo("BLEU", metrics.get(0), suffStats.get(0));
+//		printOverallInfo("METEOR", metrics.get(1), suffStats.get(1));
+//		printOverallInfo("TER", metrics.get(2), suffStats.get(2));
+//		
+//		System.err.println("Performing bootstrap resampling...");
+//		BootstrapResampler resampler = new BootstrapResampler(metrics, suffStats);
+//		List<double[]> samples = resampler.resample(numSamples);
+//		
+//		printResampleStats("BLEU", samples.get(0));
+//		printResampleStats("METEOR", samples.get(1));
+//		printResampleStats("TER", samples.get(2));
+//		
+//		saveSamples(bleuScoresOut, samples.get(0));
+//		saveSamples(meteorScoresOut, samples.get(1));
+//		saveSamples(terScoresOut, samples.get(2));
+//	}
+}
---
+++ src/multeval/significance/StratifiedApproximateRandomizationTest.java
@@ -0,0 +1,138 @@
+package multeval.significance;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.base.Preconditions;
+
+import metrics.Metric;
+import multeval.util.ArrayUtils;
+import multeval.util.SuffStatUtils;
+
+public class StratifiedApproximateRandomizationTest {
+
+	private static final Random random = new Random();
+	private final List<Metric> metrics;
+
+	private final List<List<double[]>> suffStatsA;
+	private final List<List<double[]>> suffStatsB;
+	private int totalDataPoints;
+
+	/**
+	 * @param suffStats
+	 *            First list corresponds to the metrics, the second dimension is
+	 *            number of data points (i.e. sentences) and the inner array is
+	 *            the sufficient statistics for each metric.
+	 */
+	public StratifiedApproximateRandomizationTest(List<Metric> metrics,
+			List<List<double[]>> suffStatsA, List<List<double[]>> suffStatsB) {
+
+		Preconditions.checkArgument(metrics.size() > 0, "Must have at least one metric.");
+		Preconditions.checkArgument(suffStatsA.size() > 0, "Must have at least one data point.");
+		// TODO: Check for sufficient stats and metric count under each data
+		// point being parallel (same for BootstrapResampler)
+
+		this.metrics = metrics;
+		this.suffStatsA = suffStatsA;
+		this.suffStatsB = suffStatsB;
+		this.totalDataPoints = suffStatsA.get(0).size();
+		Preconditions.checkArgument(suffStatsA.get(0).size() == suffStatsB.get(0).size(),
+				"System A and System B must have the same number of data points.");
+
+		Preconditions.checkArgument(totalDataPoints > 0, "Need more than zero data points.");
+	}
+
+	public double[] getTwoSidedP(int numShuffles) {
+
+		double[] overallDiffs = new double[metrics.size()];
+		double[] scoresA = new double[metrics.size()];
+		double[] scoresB = new double[metrics.size()];
+
+		for (int iMetric = 0; iMetric < metrics.size(); iMetric++) {
+			Metric metric = metrics.get(iMetric);
+			scoresA[iMetric] = metric.score(SuffStatUtils.sumStats(suffStatsA.get(iMetric)));
+			scoresB[iMetric] = metric.score(SuffStatUtils.sumStats(suffStatsB.get(iMetric)));
+			overallDiffs[iMetric] = Math.abs(scoresA[iMetric] - scoresB[iMetric]);
+		}
+
+		int[] diffsByChance = new int[metrics.size()];
+		boolean[] shuffling = new boolean[totalDataPoints];
+		for (int i = 0; i < numShuffles; i++) {
+			chooseShuffling(shuffling);
+			for (int iMetric = 0; iMetric < metrics.size(); iMetric++) {
+				Metric metric = metrics.get(iMetric);
+
+				double scoreX = metric.score(sumStats(shuffling, iMetric, suffStatsA, suffStatsB));
+				double scoreY = metric.score(sumStats(shuffling, iMetric, suffStatsB, suffStatsA));
+				double sampleDiff = Math.abs(scoreX - scoreY);
+				System.out.println(iMetric + ": " + scoreX + " - " + scoreY + " " + sampleDiff + " <> " + overallDiffs[iMetric]);
+				if (sampleDiff >= overallDiffs[iMetric]) {
+					diffsByChance[iMetric]++;
+				}
+			}
+		}
+
+		double[] p = new double[metrics.size()];
+		for(int iMetric = 0; iMetric < metrics.size(); iMetric++) {
+			p[iMetric] = ((double) diffsByChance[iMetric] + 1.0) / ((double) numShuffles + 1.0);
+		}
+		return p;
+	}
+
+	private double[] sumStats(boolean[] shuffling,int iMetric,
+			List<List<double[]>> suffStatsA, List<List<double[]>> suffStatsB) {
+
+		int numStats = suffStatsA.get(iMetric).get(0).length;
+		double[] summedStats = new double[numStats];
+		List<double[]> metricStatsA = suffStatsA.get(iMetric);
+		List<double[]> metricStatsB = suffStatsB.get(iMetric);
+		for (int iRow =0; iRow<metricStatsA.size();iRow++) {
+			double[] row = shuffling[iRow] ? metricStatsA.get(iRow) : metricStatsB.get(iRow);
+			ArrayUtils.plusEquals(summedStats, row);
+		}
+		return summedStats;
+	}
+
+	private static void chooseShuffling(boolean[] shuffling) {
+		for (int i = 0; i < shuffling.length; i++) {
+			shuffling[i] = random.nextBoolean();
+		}
+	}
+	
+//	public static void main(String[] args) throws IOException {
+//		if(args.length != 7) { 
+//			System.err.println("Usage: program bleuStatsA meteorStatsA terStatsA bleuStatsB meteorStatsB terStatsB numShuffles");
+//			System.exit(1);
+//		}
+//		
+//		File bleuStatsA = new File(args[0]);
+//		File meteorStatsA = new File(args[1]);
+//		File terStatsA = new File(args[2]);
+//		File bleuStatsB = new File(args[3]);
+//		File meteorStatsB = new File(args[4]);
+//		File terStatsB = new File(args[5]);
+//		int numShuffles = Integer.parseInt(args[6]);
+//		
+//		List<Metric> metrics = Arrays.asList(new BLEU(), new METEOR(METEOR.RANKING_EN_WEIGHTS), new TER());
+//		List<List<double[]>> suffStatsA = new ArrayList<List<double[]>>(metrics.size());
+//		suffStatsA.add(SuffStatUtils.loadSuffStats(bleuStatsA));
+//		suffStatsA.add(SuffStatUtils.loadSuffStats(meteorStatsA));
+//		suffStatsA.add(SuffStatUtils.loadSuffStats(terStatsA));
+//		
+//		List<List<double[]>> suffStatsB = new ArrayList<List<double[]>>(metrics.size());
+//		suffStatsB.add(SuffStatUtils.loadSuffStats(bleuStatsB));
+//		suffStatsB.add(SuffStatUtils.loadSuffStats(meteorStatsB));
+//		suffStatsB.add(SuffStatUtils.loadSuffStats(terStatsB));
+//		
+//		StratifiedApproximateRandomizationTest test = new StratifiedApproximateRandomizationTest(metrics, suffStatsA, suffStatsB);
+//		System.out.println("Running test with " + numShuffles + " shuffles");
+//		double[] p = test.getTwoSidedP(numShuffles);
+//		System.out.println("BLEU p-value: " + p[0]);
+//		System.out.println("METEOR p-value: " + p[1]);
+//		System.out.println("TER p-value: " + p[2]);
+//	}
+}
