0fd0e83b730bd094bb2aaf9b293fab352caaac2c
Jonathan Clark <jhclark@cs.cmu.edu>
2011-04-28 13:35:59 -0400

    Remove unused imports

--- src/multeval/HypothesisManager.java
+++ src/multeval/HypothesisManager.java
@@ -1,12 +1,8 @@
 package multeval;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import multeval.util.StringUtils;
