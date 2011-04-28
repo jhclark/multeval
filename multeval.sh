0206fb06523aefb22427f80546519d9579e61b40
Jonathan Clark <jhclark@cs.cmu.edu>
2011-04-28 11:27:22 -0400

    Hack around Jar files inability to properly handle class paths

--- multeval.sh
+++ multeval.sh
@@ -4,4 +4,6 @@ set -eo pipefail
 version=0.1
 scriptDir=$(dirname $0)
 $scriptDir/get_deps.sh
-java -Xmx2g -jar multeval-${version}.jar $@
+java -Xmx2g \
+    -cp $scriptDir/lib/tercom-0.7.26.jar:$scriptDir/lib/meteor-1.2/meteor-1.2.jar:$scriptDir/multeval-${version}.jar \
+    multeval.MultEval $@
