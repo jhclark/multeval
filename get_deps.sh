b5b5e6e0d573924c520cb686ac6e97c5ceab0e9e
Jonathan Clark <jhclark@cs.cmu.edu>
2011-04-29 19:43:25 -0400

    Tell user when we download

--- get_deps.sh
+++ get_deps.sh
@@ -7,6 +7,7 @@ meteorPath=$libDir/meteor-$version
 meteorUrl=http://www.cs.cmu.edu/~alavie/METEOR/download/meteor-${version}.tgz
 if [ ! -e $meteorPath ]; then
     cd $libDir
+    echo "Existing METEOR installation not found. Downloading and installing METEOR 1.2..."
     curl -L $meteorUrl | tar -xvz
 else
     echo "Found existing METEOR installation at $meteorPath"
