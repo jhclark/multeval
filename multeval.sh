d3fd194c82b2d8f701b52d67a95a54bb622eaa53
Jonathan Clark <jhclark@cs.cmu.edu>
2011-05-01 09:22:16 -0400

    Use file.encoding to fix loading meteor paraphrase tables

--- multeval.sh
+++ multeval.sh
@@ -4,4 +4,8 @@ set -eo pipefail
 version=0.1
 scriptDir=$(dirname $0)
 $scriptDir/get_deps.sh
-java -XX:+UseCompressedOops -Xmx2g -cp $scriptDir/lib/tercom-0.7.26.jar:$scriptDir/lib/meteor-1.2/meteor-1.2.jar:$scriptDir/multeval-${version}.jar multeval.MultEval $@
+
+# Specify -Dfile.encoding so that the meteor paraphrase tables load properly
+java -Dfile.encoding=UTF8 -XX:+UseCompressedOops -Xmx2g \
+    -cp $scriptDir/lib/tercom-0.7.26.jar:$scriptDir/lib/meteor-1.2/meteor-1.2.jar:$scriptDir/multeval-${version}.jar \
+    multeval.MultEval $@
