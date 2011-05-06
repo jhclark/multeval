05af3b63dd2bf89cafb7b27cd73e89be639bb82c
Jonathan Clark <jon.h.clark@gmail.com>
2011-05-06 10:43:29 -0400

    Don't pollute stdout with status messages. Use stderr.

--- get_deps.sh
+++ get_deps.sh
@@ -8,8 +8,8 @@ meteorPath=$libDir/meteor-$version
 meteorUrl=http://www.cs.cmu.edu/~alavie/METEOR/download/meteor-${version}.tgz
 if [ ! -e $meteorPath ]; then
     cd $libDir
-    echo "Existing METEOR installation not found. Downloading and installing METEOR 1.2..."
+    echo >&2 "Existing METEOR installation not found. Downloading and installing METEOR 1.2..."
     curl -L $meteorUrl | tar -xvz
 else
-    echo "Found existing METEOR installation at $meteorPath"
+    echo >&2 "Found existing METEOR installation at $meteorPath"
 fi
