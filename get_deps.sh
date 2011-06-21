ade5a8a96f0fef67c7390b9e7683eb3d06c27770
Jonathan Clark <jon.h.clark@gmail.com>
2011-06-21 15:01:08 -0400

    Fix dependency grabber

--- get_deps.sh
+++ get_deps.sh
@@ -7,9 +7,10 @@ libDir=$scriptDir/lib
 meteorPath=$libDir/meteor-$version
 meteorUrl=http://www.cs.cmu.edu/~alavie/METEOR/download/meteor-${version}.tgz
 if [ ! -e $meteorPath ]; then
+    mkdir -p $libDir
     cd $libDir
-    echo >&2 "Existing METEOR installation not found. Downloading and installing METEOR 1.2..."
-    curl -L $meteorUrl | tar -xvz
+    echo >&2 "Existing METEOR installation not found. Performing first-run setup: Downloading and installing METEOR 1.2..."
+    curl -L $meteorUrl | tar -xz
 else
     echo >&2 "Found existing METEOR installation at $meteorPath"
 fi
