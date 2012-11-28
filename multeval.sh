cccc01bcc7f8b27d6ac4eaa2599a1089407a080a
Jonathan Clark <jon.h.clark@gmail.com>
2012-11-27 21:58:28 -0500

    Dynamically choose heap size based on active metrics

--- multeval.sh
+++ multeval.sh
@@ -5,7 +5,15 @@ source $scriptDir/constants
 
 $scriptDir/get_deps.sh
 
+heap="-Xmx200m"
+for arg in "$@"; do
+    # TODO: Check if this follows --metric
+    if [[ "$arg" == "meteor" ]]; then
+        heap="-Xmx2g"
+    fi
+done
+
 # Specify -Dfile.encoding so that the meteor paraphrase tables load properly
-java -Dfile.encoding=UTF8 -XX:+UseCompressedOops -Xmx2g \
+java -Dfile.encoding=UTF8 -XX:+UseCompressedOops $heap \
     -cp $terJar:$meteorJar:$scriptDir/multeval-${version}.jar \
-    multeval.MultEval $@
+    multeval.MultEval "$@"
