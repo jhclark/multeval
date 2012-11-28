ce69d0df43a3f9cf10febad59260c38894b2cb32
Jonathan Clark <jon.h.clark@gmail.com>
2012-11-28 11:32:44 -0500

    Update runner script to choose heap size

--- multeval.sh
+++ multeval.sh
@@ -8,10 +8,12 @@ $scriptDir/get_deps.sh
 heap="-Xmx2g" # start with meteor heap size
 for arg in "$@"; do
     if [[ "$arg" == "--metrics" ]]; then
-        heap="-Xmx200m"
+        # We might not be using METEOR, so don't request any additional heap
+        heap=""
     fi
     # TODO: Check if this follows --metric
     if [[ "$arg" == "meteor" ]]; then
+        # We're using meteor after all, use the big heap
         heap="-Xmx2g"
     fi
 done
