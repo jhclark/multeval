f0457e57c5a90422e67ae4837c549c325cd02763
Jonathan Clark <jon.h.clark@gmail.com>
2012-11-28 15:51:17 -0500

    Don't use CompressedOops option, which could be causing builds to fail on the Travis VM

--- multeval.sh
+++ multeval.sh
@@ -5,20 +5,21 @@ source $scriptDir/constants
 
 $scriptDir/get_deps.sh
 
-heap="-Xmx2g" # start with meteor heap size
+METEOR_FLAGS="-XX:+UseCompressedOops -Xmx2g"
+heap="$METEOR_FLAGS" # start with meteor heap size
 for arg in "$@"; do
     if [[ "$arg" == "--metrics" ]]; then
         # We might not be using METEOR, so don't request any additional heap
-        heap=""
+        flags=""
     fi
     # TODO: Check if this follows --metric
     if [[ "$arg" == "meteor" ]]; then
         # We're using meteor after all, use the big heap
-        heap="-Xmx2g"
+        heap="$METEOR_FLAGS"
     fi
 done
 
 # Specify -Dfile.encoding so that the meteor paraphrase tables load properly
-java -Dfile.encoding=UTF8 -XX:+UseCompressedOops $heap \
+java -Dfile.encoding=UTF8 $flags \
     -cp $terJar:$meteorJar:$scriptDir/multeval-${version}.jar \
     multeval.MultEval "$@"
